7dacd54a26500820467a4fa9f016a8ecfd8463cc
Loyid Kim <thepuruya@gmail.com>
2016-05-18 02:08:42 +0900

    Let's do it 9.

--- app/src/main/java/com/loyid/weatherforecast/MainFragment.java
+++ app/src/main/java/com/loyid/weatherforecast/MainFragment.java
@@ -8,14 +8,20 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.os.SystemClock;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.CursorAdapter;
 import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -27,18 +33,11 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.loyid.weatherforecast.provider.WeatherContract;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -48,21 +47,47 @@ import java.util.List;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
-    private ArrayAdapter<String> mForecastAdapter;
+    private static final int FORECAST_LOADER = 0;
+
+    //private ArrayAdapter<String> mForecastAdapter;
+    private WeatherForecastAdapter mForecastAdapter;
     private IFetchWeatherService mService;
+
+    private static final String[] FORECAST_COLUMNS = {
+            // In this case the id needs to be fully qualified with a table name, since
+            // the content provider joins the location & weather tables in the background
+            // (both have an _id column)
+            // On the one hand, that's annoying.  On the other, you can search the weather table
+            // using the location set by the user, which is only in the Location table.
+            // So the convenience is worth it.
+            WeatherContract.WeatherColumns._ID,
+            WeatherContract.WeatherColumns.COLUMN_DATE,
+            WeatherContract.WeatherColumns.COLUMN_SHORT_DESC,
+            WeatherContract.WeatherColumns.COLUMN_MAX_TEMP,
+            WeatherContract.WeatherColumns.COLUMN_MIN_TEMP,
+    };
+
+
+    static final int COL_WEATHER_ID = 0;
+    static final int COL_WEATHER_DATE = 1;
+    static final int COL_WEATHER_DESC = 2;
+    static final int COL_WEATHER_MAX_TEMP = 3;
+    static final int COL_WEATHER_MIN_TEMP = 4;
 
     private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             if (action.equals(FetchWeatherService.ACTION_RETRIEVE_WEATHER_DATA)) {
+                /*
                 String[] data = intent.getStringArrayExtra(FetchWeatherService.EXTRA_WEATHER_DATA);
                 mForecastAdapter.clear();
                 for(String dayForecastStr : data) {
                     mForecastAdapter.add(dayForecastStr);
                 }
+                */
             }
         }
     };
@@ -88,15 +113,23 @@ public class MainFragment extends Fragment {
     private IFetchDataListener.Stub mFetchDataListener = new IFetchDataListener.Stub() {
         @Override
         public void onWeatherDataRetrieved(String[] data) throws RemoteException {
+            /*
             mForecastAdapter.clear();
             for(String dayForecastStr : data) {
                 mForecastAdapter.add(dayForecastStr);
             }
+            */
         }
     } ;
 
     public MainFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        getLoaderManager().initLoader(FORECAST_LOADER, null, this);
+        super.onActivityCreated(savedInstanceState);
     }
 
     @Override
@@ -170,12 +203,16 @@ public class MainFragment extends Fragment {
         // Now that we have some dummy forecast data, create an ArrayAdapter.
         // The ArrayAdapter will take data from a source (like our dummy forecast) and
         // use it to populate the ListView it's attached to.
+        /*
         mForecastAdapter =
                 new ArrayAdapter<String>(
                         getActivity(), // The current context (this activity)
                         R.layout.list_item_forecast, // The name of the layout ID.
                         R.id.list_item_forecast_textview, // The ID of the textview to populate.
                         weekForecast);
+        */
+
+        mForecastAdapter = new WeatherForecastAdapter(getActivity(), null, 0);
 
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
@@ -186,10 +223,12 @@ public class MainFragment extends Fragment {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                /*
                 String forecast = mForecastAdapter.getItem(position);
                 Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(getActivity(), DetailActivity.class);
                 intent.putExtra("data", forecast);
+                */
             }
         });
 
@@ -202,5 +241,108 @@ public class MainFragment extends Fragment {
     public void onDestroyView() {
         //getActivity().unregisterReceiver(mBroadcastReceiver);
         super.onDestroyView();
+    }
+
+    private class WeatherForecastAdapter extends CursorAdapter {
+        public class ViewHolder {
+            public final TextView mTextView;
+
+            public ViewHolder(View view) {
+                mTextView = (TextView)view.findViewById(R.id.list_item_forecast_textview);
+            }
+        }
+
+        public WeatherForecastAdapter(Context context, Cursor cursor, int flags) {
+            super(context, cursor, flags);
+        }
+
+        @Override
+        public View newView(Context context, Cursor cursor, ViewGroup parent) {
+            View view = LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
+
+            ViewHolder viewHolder = new ViewHolder(view);
+            view.setTag(viewHolder);
+
+            return view;
+        }
+
+        @Override
+        public void bindView(View view, Context context, Cursor cursor) {
+            ViewHolder holder = (ViewHolder)view.getTag();
+
+            StringBuilder sb = new StringBuilder();
+            long dateInMillis = cursor.getLong(COL_WEATHER_DATE);
+            sb.append(getReadableDateString(dateInMillis));
+
+            String description = cursor.getString(COL_WEATHER_DESC);
+            sb.append(" - " + description);
+
+            double high = cursor.getDouble(COL_WEATHER_MAX_TEMP);
+
+            double low = cursor.getDouble(COL_WEATHER_MIN_TEMP);
+
+            String highAndLow = formatHighLows(high, low);
+            sb.append(" - " + highAndLow);
+
+            holder.mTextView.setText(sb.toString());
+        }
+    }
+
+    private String getReadableDateString(long time){
+        // Because the API returns a unix timestamp (measured in seconds),
+        // it must be converted to milliseconds in order to be converted to valid date.
+        SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
+        return shortenedDateFormat.format(time);
+    }
+
+    private String formatHighLows(double high, double low) {
+        // For presentation, assume the user doesn't care about tenths of a degree.
+        long roundedHigh = Math.round(high);
+        long roundedLow = Math.round(low);
+
+        String highLowStr = roundedHigh + "/" + roundedLow;
+        return highLowStr;
+    }
+
+    // To make it easy to query for the exact date, we normalize all dates that go into
+    // the database to the start of the the Julian day at UTC.
+    public static long normalizeDate(long startDate) {
+        // normalize the start date to the beginning of the (UTC) day
+        Time time = new Time();
+        time.set(startDate);
+        int julianDay = Time.getJulianDay(startDate, time.gmtoff);
+        return time.setJulianDay(julianDay);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        // This is called when a new Loader needs to be created.  This
+        // fragment only uses one loader, so we don't care about checking the id.
+
+        // To only show current and future dates, filter the query to return weather only for
+        // dates after or including today.
+
+        // Sort order:  Ascending, by date.
+        String sortOrder = WeatherContract.WeatherColumns.COLUMN_DATE + " ASC";
+
+        long now = System.currentTimeMillis();
+        long normalizedDate = normalizeDate(now);
+
+        return new CursorLoader(getActivity(),
+                WeatherContract.WeatherColumns.CONTENT_URI,
+                FORECAST_COLUMNS,
+                WeatherContract.WeatherColumns.COLUMN_DATE + " >= ?",
+                new String[] {Long.toString(now)},
+                sortOrder);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mForecastAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mForecastAdapter.swapCursor(null);
     }
 }
